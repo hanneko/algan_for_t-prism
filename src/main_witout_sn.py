8f34f047c5c9922bec3feee11f14cdceeb858018
Hanneko <h-murase@ism.ac.jp>
2022-09-05 16:23:25 +0900

    Spectral Normalization未使用のdiscriminatorによる実装，異常検知notebook追加

---
+++ src/main_witout_sn.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import torch
+import torch.nn as nn
+
+import config as c
+from dataloader import make_mnist_dataloader
+from model import MLP_Discriminator_wo_SN, MLP_Generator, weights_init
+from train import train_full_gan
+
+if __name__ == "__main__":
+    z_dim = c.Z_DIM
+
+    G = MLP_Generator(z_dim=z_dim, out_dim=784)
+    D = MLP_Discriminator_wo_SN(in_dim=784)
+
+    G.apply(weights_init).to(c.DEVICE).train()
+    D.apply(weights_init).to(c.DEVICE).train()
+
+    G = nn.DataParallel(G, device_ids=[0])
+    D = nn.DataParallel(D, device_ids=[0])
+
+    print("Gen. on => {}".format(next(G.parameters()).is_cuda))
+    print("Dis. on => {}".format(next(D.parameters()).is_cuda))
+
+    if c.DEVICE == "cuda":
+        torch.backends.cudnn.benchmark = c.CUDNN_BENCH
+
+    # create Data Loader
+    dataloaders_train_val = make_mnist_dataloader(train=True)
+    dataloaders_test = make_mnist_dataloader(train=False)
+
+    # start training
+    train_full_gan(D, G, dataloaders_train_val, dataloaders_test)
+
+    print("finish training!!")
--- src/model.py
+++ src/model.py
@@ -58,6 +58,32 @@ class MLP_Discriminator(nn.Module):
         return out
 
 
+class MLP_Discriminator_wo_SN(nn.Module):
+    def __init__(self, in_dim=784):
+        super(MLP_Discriminator_wo_SN, self).__init__()
+
+        self.layer1 = nn.Sequential(
+            nn.Linear(in_dim, 1024, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+
+        self.layer2 = nn.Sequential(
+            nn.Linear(1024, 512, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+
+        self.last = nn.Sequential(
+            nn.Linear(512, 1, bias=False)
+        )
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.last(out)
+
+        return out
+
+
 def weights_init(m):
     classname = m.__class__.__name__
     if classname.find("Conv") != -1:
